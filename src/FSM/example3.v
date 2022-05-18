2eeeab197eeed4074542e27284622592935f3d7f
Emirkan Burak Yılmaz <ebylmz17@gmail.com>
2022-05-18 07:50:36 +0300

    examples

--- src/FSM/example3.v
+++ src/FSM/example3.v
@@ -1,86 +1,72 @@
 module example3(
-	input 		          		CLOCK_50,
-	input 		     [3:0]		KEY,
-	input 		          		RESET_N,
-	output		     [9:0]		LEDR,
-	input 		     [9:0]		SW
+	input CLOCK_50,
+	input[3:0] KEY,
+	input RESET_N,
+	output[9:0] LEDR,
+	input[9:0] SW
 );
 
-// ~KEY[0] : load is high
+	// Datapath Design
+	
+	// set a proper frequency clock signal
+	wire[31:0] count;
+	wire clk = count[25];
+	counter(CLOCK_50, count);
 
-wire[31:0] count;
-counter c(.clk(CLOCK_50), .count(count));
-wire clk = count[25];
+	// multiplexer inputs
+	wire d0, d1, d2;
 
-wire d0, d1;
-
-parallelReg r(clk, ~KEY[0], LEDR[4:0], d0);
-
-adder a(SW[9:5], d0, d1);
-
-mul m(
-	.d0(d0), 
-	.d1(d1), 
-	.d2(d0 ^ SW[9:5]), 
-	.d3(SW[9:5]),
-	.s(SW[1:0]),
-	.q(LEDR[4:0])
-	);
-
-
+	register(clk, ~KEY[0], LEDR[4:0], d0);
+	
+	adder(SW[9:5], d0, d1);
+	
+	shiftReg(clk, SW[2], SW[9:5], d2);
+	
+	// mux m(d0, d1, d2, SW[9:5], SW[1], SW[0], LEDR[4:0]);
+	mux m(d0, d1, d2, SW[9:5], SW[1], SW[0], LEDR[4:0]);
 endmodule
 
+
+// Datapath Component Declerations
 
 // 4x1 multiplexer
-module mul(input[4:0] d0, d1, d2, d3, input[1:0] s, output reg[4:0] q);
-	always @ (*)
-		case(s) 
-			2'b00: q <= d0;
-			2'b01: q <= d1;
-			2'b10: q <= d2;
-			2'b11: q <= d3;
-		endcase
+module mux(input[4:0] d0, d1, d2, d3, input s1, s0, output[4:0] q);
+	assign q = s1 ? (s0 ? d3 : d2) : (s0 ? d1 : d0);
 endmodule
 
-
-// module mul(input[4:0] d0, d1, d2, d3, input s1, s0, output[4:0] q);
-// 	assign q = s1 ? (s0 ? d3 : d2) : (s0 ? d1 : d0);
+// 4x1 multiplexer with always block
+// module mux(input[4:0] d0, d1, d2, d3, input[1:0] s, output reg[4:0] q);
+// 	always @ (*)
+// 		case(s)
+// 			2'b00: q <= d0;
+// 			2'b01: q <= d1;
+// 			2'b10: q <= d2;
+// 			2'b11: q <= d3;
+// 		endcase
 // endmodule
 
-
-// s: 0 (shiftLeft)
-// s: 1 (shiftRight)
-module shiftReg(input clk, s, input[4:0] d, output reg[4:0] q);
+ 
+// parallel load register
+module register(input clk, load, input[4:0] d, output reg[4:0] q);
 	always @ (posedge clk)
-		if (s)
-			q <= d >> 1;
-		else
-			q <= d << 1;
-		
+		q <= load ? d : q;
 endmodule
 
 
-// counter to get a lower frequency clock
-// count[25] becomes height in each 0.7 second
-module counter(input clk, reset, en, output reg[31:0] count);
-	always @ (posedge clk or negedge reset)
-		if (!reset)
-			count <= 32'b0;
-		else if (en)
-			count <= count + 32'b1;
-		else
-			count <= count;		
-endmodule
-
-
-// 5-bit parallel load register
-module parallelReg(input clk, load, input[4:0] d, output reg[4:0] q);
-	always @ (posedge clk)
-		q <= (load == 1) ? d : q;
-endmodule
-
-
-// 5-bit adder
-module adder(input[4:0] a, b, output[4:0] c);
+// 5 bit adder
+module adder(input[4:0] a, b, output c);
 	assign c = a + b;
+endmodule
+
+// shift register
+module shiftReg(input clk, shift_r, input[4:0] d, output reg[4:0] q);
+		always @ (posedge clk)
+			q <= (shift_r) ? d >> 1 : d << 1;
+endmodule
+
+
+// counter or timer
+module counter(input clk, output reg[31:0] count);
+	always @ (posedge clk)
+		count <= count + 32'b1; // increment by 1
 endmodule
